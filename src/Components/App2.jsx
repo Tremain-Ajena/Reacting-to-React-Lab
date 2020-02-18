3d36e861115f5600837fcf341363f93962509355
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-02-18 16:32:00 -0600

    first entry

---
+++ src/Components/App2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+displayApp(() => {
+    // let header = (<h1 className='greeting'>How Are You?</h1>);
+    return (
+        <div>
+            <h1>How Are You Now?</h1>
+
+        </div>
+    );
+});
+export default App2;
