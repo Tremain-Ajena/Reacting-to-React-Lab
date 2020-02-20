78549ddf05eaf30bbc58f88028c7caf936f8e0d2
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-02-20 10:49:57 -0600

    completed lab files

--- src/Components/App2.jsx
+++ src/Components/App2.jsx
@@ -6,10 +6,16 @@ class App2 extends Component {
         super(props);
         this.state = {
             text: 'React is difficult.',
+            // text: this.props.title,
+            // this.props.title,
             newProperty: '',
             hasLoaded: false,
         }
-        // this.text = 'Hi, thanks for the stars in the sky. We enjoy long walks in the dark and gazing at the twilight.';
+        // I set the 'text' state to the 'title' prop and removed the (props) parameter from the constructor & super and the
+        // words displayed on the page was from the 'title' prop passed through the App in the index instead of the actual
+        // string it's set to. However, when the (props) parameter is removed & the 'text' state is set to 'this.props.title'
+        // the page throws an error that says 'title of undefined cannot be read'
+        // 'states' are still 'props', they are just the state of a prop at one instance; they can be passed to a child as well.
     }
     handleInputChange(value) {
         this.setState({ newProperty: value })
@@ -18,12 +24,17 @@ class App2 extends Component {
     handleClick() {
         this.setState({ hasLoaded: true })
     }
+    componentDidMount() {
+        this.setState({ hasLoaded: true })
+    }
+    // having the 'componentDidMount' function does not allow py react page to ever change into the designated 'Loading' page
     render() {
         if (this.state.hasLoaded === false) {
             return (<div>
                 <h1>Loading</h1>
                 <button onClick={() => this.handleClick()}>Click Me!</button>
-
+                {/* the loading screen will appear first becuase the default value of hasLoaded is 'false' and clicking the button changes the value to 'true' */}
+                {/* <button onClick={() => this.componentDidMount()}>Click Me!</button> */}
             </div>)
         } else {
 
@@ -42,6 +53,8 @@ class App2 extends Component {
                         onChange={(event) => this.handleInputChange(event.target.value)}
                     />
                     <button onClick={() => this.handleClick()}>Click Me!</button>
+                    {/* <button onClick={() => this.componentDidMount()}>Click Me!</button> */}
+                    {/* the loading page does not appear when invoking the 'componentDidMount' function for the button event listener */}
                 </React.Fragment>
             )
         }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App2 from './Components/App2';
 ReactDOM.render(
     // <App />,
     <App2
-    title = {'Hi, thanks for the stars in the sky. We enjoy long walks in the dark and gazing at the twilight.'}
+        title={'Hi, thanks for the stars in the sky. We enjoy long walks in the dark and gazing at the twilight.'}
     />,
     document.getElementById('root')
 );
